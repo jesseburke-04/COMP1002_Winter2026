43c021127a4948c57f5f4754d141098677db0256
jesseburke-04 <jesseburke04@gmail.com>
2026-03-26 19:59:16 -0400

    changes

--- lab_four/script.js
+++ lab_four/script.js
@@ -1,6 +1,23 @@
+//Lab Four Challenge
+//Submit Button: Find ID then wait until submit is triggered.
 document.getElementById("welcomeForm").addEventListener("submit", function(event) {
+    //make sure the page doesn't refresh
     event.preventDefault();
-
+    //use a variable to store users input
     let name = document.getElementById("nameInput").value;
+    //change the text and call the users input
     document.getElementById("welcomeMessage").textContent = "Welcome, " + name + "!";
+  });
+
+//Theme Button: Find ID then wait until click is triggered.
+document.getElementById("themeToggle").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        this.textContent = "Toggle Light Mode";
+    }else {
+        this.textContent = "Toggle Dark Mode";
+    }
 });
+
+
+
